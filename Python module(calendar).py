f97f249d8fda00a984351140bd483b7cd60ffc0a
rmdevavignesh <rmdevavignesh@gmail.com>
2026-07-02 12:50:00 +0530

    Add files via upload

---
+++ Python module(calendar).py
@@ -0,0 +1,20 @@
+'''import calendar
+#calendar modules
+print(calendar.day_name[0])
+
+print(calendar.month_name[4])
+
+print(calendar.weekday(2026,8,7))
+
+print(calendar.leapdays(1996,2026))
+
+print(calendar.isleap(1992))
+print(calendar.isleap(1998))
+
+print(calendar.monthrange(2026,5))
+
+print(calendar.month(2026,5))
+
+print(calendar.calendar(2026))'''
+
+
---
+++ Python modules(math).py
@@ -0,0 +1,124 @@
+'''#math function
+#1(a)
+import math
+print(math.sqrt(64))
+
+#1(b)
+import math
+print(math.lcm(4,6))
+
+#1(c)
+import math
+print(math.gcd(4,6))
+
+#1(d)
+import math
+print(math.factorial(8))
+
+#1(e)
+import math
+print(math.fabs(-9))
+
+#1(f)
+import math
+a=3
+b=5
+math.pow(a,b)
+print(math.pow(a,b))
+
+#2(a)
+import math
+print(math.pi)
+
+#2(b)
+import math
+print(math.tau)
+
+#2(c)
+import math
+print(math.e)
+
+#2(d)
+import math
+print(math.inf)
+
+#2(e)
+import math
+print(math.nan)
+
+import math
+#3(a)
+print(math.ceil(4.3))
+
+#3(b)
+print(math.floor(3.9))
+
+#3(c)
+print(math.trunc(3.2))
+
+#3(d)
+x=3.14159265
+n=3
+print(round(x,n))
+
+#3(e)
+x=float(input("Enter value:"))
+print(math.modf(x))
+
+import math
+#4(a)
+print(math.radians(60))
+
+#4(b)
+print(math.degrees((3.14)))
+
+import math
+#5(a)
+print(math.log(10,10))
+
+#5(b)
+print(math.log10(1000))
+
+#5(c)
+print(math.log2(10))
+
+#5(d)
+print(math.exp(1))
+
+#5(e)
+x=1
+print(math.expm1(x))
+
+import math
+#6(a)
+print(math.sin(math.pi/4))
+print(math.sin(0))
+
+#6(b)
+print(math.cos(0))
+
+#6(c)
+print(math.tan(math.pi/2))
+
+#6(d)
+print(math.asin(1))
+
+#6(d)
+print(math.acos(1))
+
+#6(d)
+print(math.atan(1))
+
+import math
+#7(a)
+print(math.sinh(2))
+
+#7(b)
+print(math.tanh(0))
+
+#7(c)
+print(math.cosh(1))'''
+
+
+
+
