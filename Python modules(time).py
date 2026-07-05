76a2cc26c5351501c34d9ac50c2d070ed472346d
rmdevavignesh <rmdevavignesh@gmail.com>
2026-07-05 11:26:59 +0530

    Add files via upload

---
+++ Python modules(time).py
@@ -0,0 +1,29 @@
+'''import time
+#time modules
+print(time.time())
+
+print(time.ctime())
+
+print(time.localtime())
+
+print(time.asctime())
+
+print(time.gmtime())
+
+print(time.mktime(time.localtime()))
+
+print(time.perf_counter())
+
+print(time.strftime("%d@%m@%y"))
+
+print(time.strftime("%d.%m.%y"))
+
+print(time.strftime("%d-%m-%y"))
+
+import datetime
+print(datetime.datetime.now())
+
+print(datetime.date.today())
+
+print(datetime.datetime.strptime("05.07.2026","%d.%m.%Y"))'''
+
